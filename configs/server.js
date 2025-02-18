f88460ab42fe25da61baa7508e7a32a7d7ae1fbb
Cristian Luna <cluna-2023308@kinal.edu.gt>
2025-02-18 11:04:33 -0600

    add: modulo se user culminado

--- configs/server.js
+++ configs/server.js
@@ -1,7 +1,29 @@
 "use strict"
 
 import express from "express"
+import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan"
 import { dbConnection } from "./mongo.js"
+import authRoutes from "../src/auth/auth.routes.js"
+import userRoutes from "../src/user/user.routes.js"
+import apiLimiter from "../src/middlewares/rate-limit-validator.js"
+import createAdmin from "../src/user/user.controller.js"
+
+const middlewares = (app) => {
+    app.use(express.urlencoded({extended: false}))
+    app.use(express.json())
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgan("dev"))
+    app.use(apiLimiter)
+}
+
+const routes = (app) =>{
+    app.use("/opinionSystem/v1/auth", authRoutes)
+    app.use("/opinionSystem/v1/user", userRoutes)
+}
+
 
 const conectarDB = async () => {
     try{
@@ -15,6 +37,9 @@ export const initServer = () => {
     const app = express()
     try{
         conectarDB()
+        conectarDB()
+        routes(app)
+        createAdmin()
         app.listen(process.env.PORT)
         console.log(`Server running on port ${process.env.PORT}`)
     }catch(err){
---
+++ src/auth/auth.routes.js
@@ -0,0 +1,22 @@
+import { Router } from "express"
+import { register, login} from "./auth.controller.js"
+import { registerValidator, loginValidator, assignUserRole } from "../middlewares/user-validators.js"
+import { uploadProfilePicture } from "../middlewares/multer-uploads.js"
+
+const router = Router()
+
+router.post(
+    "/register",
+    uploadProfilePicture.single("profilePicture"), 
+    registerValidator, 
+    assignUserRole,
+    register
+)
+
+router.post(
+    "/login",
+    loginValidator,
+    login
+)
+
+export default router
